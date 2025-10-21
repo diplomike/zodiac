b97868d7adf493380ddc21fbdf31a93472c59e29
diplomike <diplomike@gmail.com>
2025-10-21 15:58:12 +0800

    Initial commit

---
+++ Zodiac signs.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+zodiac <- c(1:12)
+s <- sample(zodiac,300,T)
+acc <- NULL
+pred <- NULL
+for (i in 1:1000) {
+  for (n in 1:300)
+    pred <-  c(pred, s[n] == sample(zodiac,1))
+  acc <- c(acc, mean(pred))
+  pred <- NULL}
+cat ("expected acc = ", 1/12)
+ggplot(data.frame(acc)) + geom_histogram(aes(acc), bins=50) + 
+  scale_x_continuous("Prediction accuracy", seq(0,,0.005),seq(0,,0.001),,seq(0,,0.005))
+
+quantile(acc, 0.99)

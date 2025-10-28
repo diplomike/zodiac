fd7c2fb596a98968eabfcace811e9d1662803c61
diplomike <diplomike@gmail.com>
2025-10-29 04:50:07 +0800

    Final commit

--- Zodiac signs.R
+++ Zodiac signs.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(usethis) 'For using use_github'
 
 zodiac <- c(1:12)
 s <- sample(zodiac,300,T)
@@ -14,3 +15,5 @@ ggplot(data.frame(acc)) + geom_histogram(aes(acc), bins=50) +
   scale_x_continuous("Prediction accuracy", seq(0,,0.005),seq(0,,0.001),,seq(0,,0.005))
 
 quantile(acc, 0.99) 'Indicate the accuracy above which the population only constitutes to 1% of the total.'
+
+use_github() 'To create a remote repo on Github.'

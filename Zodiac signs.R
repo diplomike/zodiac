a7e7beef203c2564fa85165a3a73ef7a165e19ac
diplomike <diplomike@gmail.com>
2025-10-29 04:51:36 +0800

    Finished

--- Zodiac signs.R
+++ Zodiac signs.R
@@ -1,5 +1,6 @@
 library(tidyverse)
-library(usethis) 'For using use_github'
+library(usethis)
+'For using use_github'
 
 zodiac <- c(1:12)
 s <- sample(zodiac,300,T)
@@ -14,6 +15,8 @@ cat ("expected acc = ", 1/12)
 ggplot(data.frame(acc)) + geom_histogram(aes(acc), bins=50) + 
   scale_x_continuous("Prediction accuracy", seq(0,,0.005),seq(0,,0.001),,seq(0,,0.005))
 
-quantile(acc, 0.99) 'Indicate the accuracy above which the population only constitutes to 1% of the total.'
+quantile(acc, 0.99) 
+'Indicate the accuracy above which the population only constitutes to 1% of the total.'
 
-use_github() 'To create a remote repo on Github.'
+use_github() 
+'To create a remote repo on Github.'
